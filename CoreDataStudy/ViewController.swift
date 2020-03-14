5cc9da455dfef910336565c945136aeb9242743f
김정민 <dsaqaz421@naver.com>
2020-03-14 19:00:42 +0900

    Initial Commit

---
+++ CoreDataStudy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataStudy
+//
+//  Created by 김정민 on 2020/03/14.
+//  Copyright © 2020 kjm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
