b92aab955392323891b8e00895d67c9e2d8ad432
김정민 <dsaqaz421@naver.com>
2020-03-29 16:37:26 +0900

    fetched results 복습하자

---
+++ CoreDataStudy/FetchedResultsController/FetchedResultsControllerTableViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FetchedResultsControllerTableViewController.swift
+//  CoreDataStudy
+//
+//  Created by 김정민 on 2020/03/29.
+//  Copyright © 2020 kjm. All rights reserved.
+//
+
+import UIKit
+
+class FetchedResultsControllerTableViewController: UITableViewController {
+
+    /*
+        Fetched Results Controller
+     1. 
+     
+     */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
